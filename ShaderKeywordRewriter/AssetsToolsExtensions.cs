70b4906cc0bf6219d72264c78f71d3f99f079571
Nicolas Gnyra <nicolas@gnyra.com>
2023-10-11 19:04:23 -0400

    WIP

---
+++ ShaderKeywordRewriter/AssetsToolsExtensions.cs
@@ -0,0 +1,100 @@
+﻿using AssetsTools.NET;
+using AssetsTools.NET.Extra;
+
+namespace ShaderKeywordRewriter
+{
+    internal static class AssetsToolsExtensions
+    {
+        internal static void PrintFieldNodes(this AssetsFile assetsFile, string fieldName)
+        {
+            TypeTreeType typeTreeType = assetsFile.Metadata.FindTypeTreeTypeByID((int)AssetClassID.Material);
+
+            void PrintNode(int index)
+            {
+                TypeTreeNode node = typeTreeType.Nodes[index];
+                Console.WriteLine($"AddNode(typeTreeType, {node.ByteSize}, {node.Level}, 0x{node.MetaFlags:X}, \"{node.GetNameString(typeTreeType.StringBuffer)}\", {node.RefTypeHash}, {string.Join(" | ", Enum.GetValues<TypeTreeNodeFlags>().Where(f => (node.TypeFlags.HasFlag(f) && f != 0) || (f == 0 && node.TypeFlags == 0)).Select(f => $"TypeTreeNodeFlags.{f}"))}, \"{node.GetTypeString(typeTreeType.StringBuffer)}\", {node.Version});");
+            }
+
+            int start = typeTreeType.Nodes.FindIndex(n => n.GetNameString(typeTreeType.StringBuffer) == fieldName)!;
+            PrintNode(start);
+
+            for (int i = start + 1; typeTreeType.Nodes[i].Level > typeTreeType.Nodes[start].Level; ++i)
+            {
+                PrintNode(i);
+            }
+        }
+
+        internal static void InitializeField(this AssetTypeValueField valueField, TypeTreeType typeTreeType, string fieldName)
+        {
+            AssetTypeTemplateField templateField = new();
+            templateField.FromTypeTree(typeTreeType, fieldName);
+
+            valueField.TemplateField.Children.Add(templateField);
+            valueField.Children.Add(ValueBuilder.DefaultValueFieldFromTemplate(templateField));
+        }
+
+        internal static void AppendNode(this TypeTreeType typeTreeType, int byteSize, byte level, uint metaFlags, string nameStr, ulong refTypeHash, TypeTreeNodeFlags typeFlags, string typeStr, ushort version)
+        {
+            uint nameStrOffset = typeTreeType.GetStringOffset(nameStr);
+            uint typeStrOffset = typeTreeType.GetStringOffset(typeStr);
+
+            typeTreeType.Nodes.Add(new TypeTreeNode
+            {
+                ByteSize = byteSize,
+                Index = (uint)typeTreeType.Nodes.Count,
+                Level = level,
+                MetaFlags = metaFlags,
+                NameStrOffset = nameStrOffset,
+                RefTypeHash = refTypeHash,
+                TypeFlags = typeFlags,
+                TypeStrOffset = typeStrOffset,
+                Version = version,
+            });
+        }
+
+        internal static void FromTypeTree(this AssetTypeTemplateField assetTypeTemplateField, TypeTreeType typeTreeType, string fieldName)
+        {
+            int startIndex = typeTreeType.Nodes.FindIndex(n => n.GetNameString(typeTreeType.StringBuffer) == fieldName);
+
+            if (startIndex == -1)
+            {
+                throw new InvalidOperationException($"No field named '{fieldName}'");
+            }
+
+            int endIndex = typeTreeType.Nodes.FindIndex(startIndex + 1, n => n.Level == typeTreeType.Nodes[startIndex].Level);
+
+            if (endIndex == -1)
+            {
+                endIndex = typeTreeType.Nodes.Count;
+            }
+
+            TypeTreeType dummy = new()
+            {
+                Nodes = new List<TypeTreeNode>(typeTreeType.Nodes.Skip(startIndex).Take(endIndex - startIndex)),
+                StringBufferBytes = typeTreeType.StringBufferBytes,
+            };
+
+            assetTypeTemplateField.FromTypeTree(dummy);
+        }
+
+        private static uint GetStringOffset(this TypeTreeType typeTreeType, string str)
+        {
+            int index = TypeTreeType.COMMON_STRING_TABLE.IndexOf(str + '\0', StringComparison.Ordinal);
+
+            if (index != -1)
+            {
+                return (uint)index | 0x80000000;
+            }
+
+            index = typeTreeType.StringBuffer.IndexOf(str + '\0', StringComparison.Ordinal);
+
+            if (index == -1)
+            {
+                index = typeTreeType.StringBuffer.Length;
+                typeTreeType.StringBuffer += $"{str}\0";
+            }
+
+            return (uint)index;
+        }
+    }
+}
---
+++ ShaderKeywordRewriter/Program.cs
@@ -0,0 +1,156 @@
+﻿using AssetsTools.NET;
+using AssetsTools.NET.Extra;
+using ShaderKeywordRewriter;
+
+string filePath = args[0];
+
+Console.WriteLine($"Loading asset bundle from '{filePath}'");
+
+var manager = new AssetsManager();
+
+using FileStream readStream = File.OpenRead(filePath);
+var bundleInstance = manager.LoadBundleFile(readStream, false);
+
+Console.WriteLine("Bundle created in Unity " + bundleInstance.file.Header.EngineVersion);
+
+AssetBundleCompressionType compressionType = AssetBundleCompressionType.None;
+
+if (bundleInstance.file.DataIsCompressed)
+{
+    compressionType = bundleInstance.file.GetCompressionType();
+    Console.WriteLine($"Decompressing using {compressionType}");
+    bundleInstance.file = BundleHelper.UnpackBundle(bundleInstance.file);
+}
+
+var fileIndex = 0;
+var assetsFileInstance = manager.LoadAssetsFileFromBundle(bundleInstance, fileIndex, false);
+var assetsFile = assetsFileInstance.file;
+
+// assetsFile.PrintFieldNodes("m_ValidKeywords");
+// assetsFile.PrintFieldNodes("m_InvalidKeywords");
+
+TypeTreeType typeTreeType = assetsFile.Metadata.FindTypeTreeTypeByID((int)AssetClassID.Material);
+TypeTreeType typeTreeTypeWorkingCopy = new()
+{
+    Nodes = new List<TypeTreeNode>(typeTreeType.Nodes),
+    StringBufferBytes = typeTreeType.StringBufferBytes,
+};
+
+typeTreeTypeWorkingCopy.AppendNode(-1, 1, 0x8000, "m_ValidKeywords", 0, TypeTreeNodeFlags.None,  "vector", 1);
+typeTreeTypeWorkingCopy.AppendNode(-1, 2, 0xC000, "Array",           0, TypeTreeNodeFlags.Array, "Array",  1);
+typeTreeTypeWorkingCopy.AppendNode(4,  3, 0,      "size",            0, TypeTreeNodeFlags.None,  "int",    1);
+typeTreeTypeWorkingCopy.AppendNode(-1, 3, 0x8000, "data",            0, TypeTreeNodeFlags.None,  "string", 1);
+typeTreeTypeWorkingCopy.AppendNode(-1, 4, 0x4001, "Array",           0, TypeTreeNodeFlags.Array, "Array",  1);
+typeTreeTypeWorkingCopy.AppendNode(4,  5, 0x0001, "size",            0, TypeTreeNodeFlags.None,  "int",    1);
+typeTreeTypeWorkingCopy.AppendNode(1,  5, 0x0001, "data",            0, TypeTreeNodeFlags.None,  "char",   1);
+
+typeTreeTypeWorkingCopy.AppendNode(-1, 1, 0x8000, "m_InvalidKeywords", 0, TypeTreeNodeFlags.None,  "vector", 1);
+typeTreeTypeWorkingCopy.AppendNode(-1, 2, 0xC000, "Array",             0, TypeTreeNodeFlags.Array, "Array",  1);
+typeTreeTypeWorkingCopy.AppendNode(4,  3, 0,      "size",              0, TypeTreeNodeFlags.None,  "int",    1);
+typeTreeTypeWorkingCopy.AppendNode(-1, 3, 0x8000, "data",              0, TypeTreeNodeFlags.None,  "string", 1);
+typeTreeTypeWorkingCopy.AppendNode(-1, 4, 0x4001, "Array",             0, TypeTreeNodeFlags.Array, "Array",  1);
+typeTreeTypeWorkingCopy.AppendNode(4,  5, 0x0001, "size",              0, TypeTreeNodeFlags.None,  "int",    1);
+typeTreeTypeWorkingCopy.AppendNode(1,  5, 0x0001, "data",              0, TypeTreeNodeFlags.None,  "char",   1);
+
+bool anyKeywordsUpdated = false;
+
+Console.WriteLine("Updating materials");
+
+foreach (AssetFileInfo materialInfo in assetsFile.GetAssetsOfType(AssetClassID.Material))
+{
+    AssetTypeValueField materialBaseField = manager.GetBaseField(assetsFileInstance, materialInfo);
+
+    Console.WriteLine("-> " + materialBaseField["m_Name"].AsString);
+
+    materialBaseField.InitializeField(typeTreeTypeWorkingCopy, "m_ValidKeywords");
+    materialBaseField.InitializeField(typeTreeTypeWorkingCopy, "m_InvalidKeywords");
+
+    var shaderKeywords = materialBaseField["m_ShaderKeywords"].AsString.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    
+    AssetTypeValueField validKeywordsArray = materialBaseField["m_ValidKeywords.Array"];
+
+    foreach (string shaderKeyword in shaderKeywords)
+    {
+        Console.WriteLine("--> " + shaderKeyword);
+
+        AssetTypeValueField arrayValue = ValueBuilder.DefaultValueFieldFromArrayTemplate(validKeywordsArray);
+        arrayValue.AsString = shaderKeyword;
+        validKeywordsArray.Children.Add(arrayValue);
+        anyKeywordsUpdated = true;
+    }
+
+    materialInfo.SetNewData(materialBaseField);
+}
+
+AssetPPtr? avatarDescriptorPointer = null;
+List<AssetPPtr> scriptTypes = assetsFile.Metadata.ScriptTypes;
+for (int i = 0; i < scriptTypes.Count; i++)
+{
+    AssetTypeReference assetsFileScriptInfo = AssetHelper.GetAssetsFileScriptInfo(manager, assetsFileInstance, i);
+    if (assetsFileScriptInfo != null && assetsFileScriptInfo.AsmName == "CustomAvatar.dll" && assetsFileScriptInfo.Namespace == "CustomAvatar" && assetsFileScriptInfo.ClassName == "AvatarDescriptor")
+    {
+        avatarDescriptorPointer = scriptTypes[i];
+        break;
+    }
+}
+
+foreach (AssetFileInfo monoBehaviourInfo in assetsFile.GetAssetsOfType(AssetClassID.MonoBehaviour))
+{
+    AssetTypeValueField monoBehaviourBaseField = manager.GetBaseField(assetsFileInstance, monoBehaviourInfo);
+
+    if (monoBehaviourBaseField["m_Script.m_FileID"].AsLong != avatarDescriptorPointer.FileId || monoBehaviourBaseField["m_Script.m_PathID"].AsLong != avatarDescriptorPointer.PathId)
+    {
+        continue;
+    }
+
+    monoBehaviourBaseField["author"].Value.AsString += " [updated by ShaderKeywordRewriter]";
+    monoBehaviourInfo.SetNewData(monoBehaviourBaseField);
+}
+
+if (!anyKeywordsUpdated)
+{
+    Console.WriteLine("No shader keywords found in any materials; no changes needed");
+    return;
+}
+
+typeTreeType.Nodes[0].Version = 8; // necessary for new fields to be read - doesn't seem to affect loading in 2019
+typeTreeType.Nodes = typeTreeTypeWorkingCopy.Nodes;
+typeTreeType.StringBufferBytes = typeTreeTypeWorkingCopy.StringBufferBytes;
+
+bundleInstance.file.BlockAndDirInfo.DirectoryInfos[fileIndex].SetNewData(assetsFile);
+
+Console.WriteLine("Writing updated data");
+
+string tempPath = Path.GetTempFileName();
+using (FileStream writeStream = File.Open(tempPath, FileMode.Create, FileAccess.Write, FileShare.None))
+using (AssetsFileWriter writer = new(writeStream))
+{
+    // Pack doesn't use content replacers so we need to write uncompressed first
+    bundleInstance.file.Write(writer);
+}
+
+void CompressToFile(string sourcePath, string targetPath)
+{
+    AssetBundleFile compressedBundle = new();
+
+    using (FileStream readStream = File.OpenRead(sourcePath))
+    using (AssetsFileReader reader = new(readStream))
+    {
+        compressedBundle.Read(reader);
+
+        using (FileStream writeStream = File.Open(targetPath, FileMode.Create, FileAccess.Write, FileShare.None))
+        using (AssetsFileWriter writer = new(writeStream))
+        {
+            // LZMA is the modern default
+            compressedBundle.Pack(writer, compressionType);
+        }
+    }
+
+    compressedBundle.Close();
+}
+
+string targetPath = Path.ChangeExtension(filePath, ".mod.avatar");
+Console.WriteLine($"Compressing bundle and saving to '{targetPath}'");
+CompressToFile(tempPath, targetPath);
+
+File.Delete(tempPath);
